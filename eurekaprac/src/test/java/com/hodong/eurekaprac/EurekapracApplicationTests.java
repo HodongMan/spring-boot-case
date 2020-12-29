bf39b4bb53b228380aa43157c18f71e963ae6035
hodongman <jhd9206@gmail.com>
2020-12-29 12:06:50 +0900

    eureka server init update

---
+++ eurekaprac/src/test/java/com/hodong/eurekaprac/EurekapracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodong.eurekaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekapracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
